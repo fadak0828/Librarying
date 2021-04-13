30db9e60b4c57f16386e93982e39d30b9fd81c83
heyou32 <heyou32@gmail.com>
2021-04-13 19:57:12 +0900

    집이랑 공룡알

--- Assets/Scripts/KnockEffect.cs
+++ Assets/Scripts/KnockEffect.cs
@@ -7,15 +7,28 @@ public class KnockEffect : MonoBehaviour
     AudioSource knockSound;
     Animator doorAnim;
     public GameObject doorPivot;
+    public GameObject hand;
+    public GameObject handPivot;
+    Animator handAnim;
     void Start()
     {
         knockSound = GetComponent<AudioSource>();
         doorAnim = doorPivot.GetComponent<Animator>();
+        handAnim = handPivot.GetComponent<Animator>();
         doorAnim.enabled = false;
+        hand.SetActive(false);
     }
     private void OnTriggerEnter(Collider other)
     {
+        other.GetComponent<MeshRenderer>().enabled = false;
         knockSound.Play();
+        hand.SetActive(true);
+        Invoke("DoorOpen", 1.7f);
+    }
+    void DoorOpen()
+    {
         doorAnim.enabled = true;
+        handAnim.enabled = false;
+        knockSound.Stop();
     }
 }
