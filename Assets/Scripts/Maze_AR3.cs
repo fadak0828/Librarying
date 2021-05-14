be4416c2471381179fb62cb3d920fefa72b036a8
heyou32 <heyou32@gmail.com>
2021-05-14 10:38:56 +0900

    AR3 Maze_AR3스크립트 / 에니메이션 수정

--- Assets/Scripts/Maze_AR3.cs
+++ Assets/Scripts/Maze_AR3.cs
@@ -7,13 +7,15 @@ public class Maze_AR3 : MonoBehaviour
     public GameObject ww;
     public GameObject sh;
     public GameObject startLine;
+    public GameObject finishPosition;
     bool move;
+    bool finish;
     Rigidbody rig;
     private void Start()
     {
         Invoke("Move", 2.1f);
         rig = GetComponent<Rigidbody>();
-        Invoke("StartLine", 3);
+        Invoke("StartLine", 3.5f);
     }
     void Update()
     {
@@ -22,18 +24,24 @@ public class Maze_AR3 : MonoBehaviour
             rig.MovePosition(transform.position + transform.forward * Time.deltaTime * .5f);
             //transform.Translate(Vector3.forward * Time.deltaTime * .5f);
         }
+        if(finish==true)
+            transform.position = Vector3.MoveTowards(transform.position, finishTarget.transform.position, .2f * Time.deltaTime);
     }
     private void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.CompareTag("Wall")) move = false;
     }
+    public GameObject finishTarget;
     private void OnTriggerEnter(Collider other)
     {
         if (other.name == "FinishLine")       //�̷� Ż�� ���ϸ��̼�     //��ƼŬ �߰��ϱ�
         {
-            ww.GetComponent<Animator>().Play("T-Rex_Roar 1");
-            sh.GetComponent<Animator>().Play("SHFinished");
+            ww.GetComponent<Animator>().SetTrigger("Finish");
+            sh.GetComponent<Animator>().SetTrigger("Finish");
+            GetComponent<Animator>().Play("MazeClear");
             move = false;
+            finish = true;
+            finishPosition.SetActive(true);            
         }
     }
 
@@ -46,11 +54,12 @@ public class Maze_AR3 : MonoBehaviour
     void Move()
     {
         move = true;
+        print(0);
     }
 
     void StartLine()
     {
-        startLine.SetActive(true);
+        startLine.GetComponent<BoxCollider>().enabled = true;
     }
 }
 
