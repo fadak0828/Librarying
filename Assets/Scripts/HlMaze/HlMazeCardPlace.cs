764f0a3ceb001580641256530ed18f08f920bbf0
Minwoo Song <asd0828@gmail.com>
2021-05-20 14:05:19 +0900

    더빙 오디오 클립 추가

--- Assets/Scripts/HlMaze/HlMazeCardPlace.cs
+++ Assets/Scripts/HlMaze/HlMazeCardPlace.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class HlMazeCardPlace : CardPlacement
 {
     private Maze_AR3 maze_AR3;
+    public List<AudioClip> clips;
+    public AudioSource audioSource;
 
     private void Start() {
         maze_AR3 = GetComponent<Maze_AR3>();
@@ -14,5 +16,11 @@ public class HlMazeCardPlace : CardPlacement
     {
         base.OnCardPlacement();
         maze_AR3.RotateTo(placedCardDirection);
+
+        if (!audioSource.isPlaying) {
+            audioSource.Stop();
+            audioSource.clip = clips[Random.Range(0, clips.Count)];
+            audioSource.Play();
+        }
     }
 }
