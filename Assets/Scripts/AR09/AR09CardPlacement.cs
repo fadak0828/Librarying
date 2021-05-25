6ccbb5363e76c6a772da981fec27f768fd2b1c66
Minwoo Song <asd0828@gmail.com>
2021-05-25 18:48:15 +0900

    구멍, 노크 더빙 추가

--- Assets/Scripts/AR09/AR09CardPlacement.cs
+++ Assets/Scripts/AR09/AR09CardPlacement.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class AR09CardPlacement : CardPlacement
 {
+    public static int knockCount = 0;
+    public AudioSource audioSource;
     private KnockEffect knock;
 
     protected override void OnEnable() {
         base.OnEnable();
         knock = GetComponent<KnockEffect>();
+        knockCount = 0;
     }
 
     protected override void OnCardPlacement()
@@ -16,5 +19,19 @@ public class AR09CardPlacement : CardPlacement
         base.OnCardPlacement();
         print("KnockCard Placed");
         knock.PlayKnock();
+        knockCount++;
+        if (knockCount == 3) {
+            Invoke("PlayVoice", 4);
+            Invoke("ShowNextPageUI", 8);
+        }
+    }
+
+    private void PlayVoice() {
+        audioSource.Stop();
+        audioSource.Play();
+    }
+
+    private void ShowNextPageUI() {
+        OnboardingUIManager.Instance.ShowUiByName("FindNextPage");
     }
 }
